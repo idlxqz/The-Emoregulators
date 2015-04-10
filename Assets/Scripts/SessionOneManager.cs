cb81de6378a0c8dbbfd206b1759c2468d1c247f9
João Miguel Dias <joao.dias@gaips.inesc-id.pt>
2015-04-10 19:14:25 +0100

    no message

--- Assets/Scripts/SessionOneManager.cs
+++ Assets/Scripts/SessionOneManager.cs
@@ -149,7 +149,7 @@ public class SessionOneManager : MonoBehaviour {
                     memeter.enabled = true;
                     currentState = SessionState.MeMeterReuse;
                 };
-                customText.Setup(setupNextPhase, 20, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book");
+                customText.Setup(setupNextPhase, Constants.TextTimeToDisplay, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book");
                 customText.enabled = true;
                 currentState = SessionState.CustomText;
             }
@@ -184,7 +184,7 @@ public class SessionOneManager : MonoBehaviour {
                     candle.enabled = true;
                     currentState = SessionState.CloseSession;
                 };
-                customText.Setup(setupNextPhase, 20, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book");
+                customText.Setup(setupNextPhase, Constants.TextTimeToDisplay, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book");
                 customText.enabled = true;
                 currentState = SessionState.CustomText;
             }
