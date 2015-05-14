548fa0ca30c30dd09e12699528a9d5b913952883
Goncalo Pereira <gdgp13@gmail.com>
2015-05-14 02:05:41 +0100

    - added basic animation for the custom title script

--- Assets/Scripts/CustomTitleScript.cs
+++ Assets/Scripts/CustomTitleScript.cs
@@ -8,9 +8,15 @@ public class CustomTitleScript : MonoBehaviour {
     public int titleDuration;
     private float titleStart;
     public System.Action setupNextPhase;
+    private int lastChar;
+    private float loadCharTimer;
+    public float charLoadInterval;
+    private bool loading;
 
     //information
     private string title;
+    private string loadedText;
+    private char[] charsToShow;
 
     //formating
     public int titleLateralPadding;
@@ -28,11 +34,23 @@ public class CustomTitleScript : MonoBehaviour {
 
     void OnGUI()
     {
-        if ((Time.time - titleStart) < titleDuration && !finished)
+        if (lastChar < charsToShow.Length && (Time.time - loadCharTimer) > charLoadInterval)
+        {
+            //load another char
+            loadedText += charsToShow[lastChar++];
+            //check if loading is done
+            if (lastChar == charsToShow.Length)
+            {
+                titleStart = Time.time; // start final countdown
+                loading = false;
+            }
+            loadCharTimer = Time.time;
+        }
+        if (loading || ((Time.time - titleStart) < titleDuration && !finished))
         {   
             GUI.Label(
                     new Rect(titleLateralPadding, Screen.height / 2 - titleRectHeight / 2, Screen.width - titleLateralPadding * 2, titleRectHeight),
-                    title,
+                    loadedText,
                     titleFormat);
         }
         else
@@ -45,6 +63,10 @@ public class CustomTitleScript : MonoBehaviour {
         setupNextPhase = _nextPhase;
         finished = false;
         title = _title;
-        titleStart = Time.time;
+        lastChar = 0;
+        charsToShow = title.ToCharArray();
+        loading = true;
+        loadedText = "";
+        loadCharTimer = Time.time;
     }
 }
